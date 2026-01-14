c925c85a19ec9f2d1dfeeec6b09dabff5b16ce70
Maksym Malyi <malyimaksym37@gmail.com>
2026-01-14 10:06:42 +0200

    sum from array of int

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 
    + Напишіть метод, який приймає ціле число як аргумент і виводить його квадрат.
    + Напишіть метод, який приймає два аргументи типу double - радіус та висоту - і повертає об'єм циліндра. Виведіть об'єм циліндра на екран.
-    Напишіть метод, який приймає масив цілих чисел та обчислює і повертає суму всіх елементів масиву.
+   + Напишіть метод, який приймає масив цілих чисел та обчислює і повертає суму всіх елементів масиву.
     Напишіть метод, який приймає рядок (String) як аргумент та повертає новий рядок, який складається з букв цього рядка у зворотньому порядку.
     Напишіть метод, який приймає два цілих числа, a та b, і повертає результат a^b (a підняте до степеня b).
     Напишіть метод, який приймає параметри: ціле число n і рядок text. Метод повинен вивести текстовий рядок text n рази, розділяючи кожен рядок символом переносу.
@@ -17,12 +17,23 @@ void main() {
     var radius = 3.5;
     var height = 10.0;
     var volume = getVolume(height, radius);
+    var arrayOfInt = new int[] { 10, 20, 30, 40, 50};
+    var sumFromArrayOfInt = getSumOfAllIntInTheArray(arrayOfInt);
     System.out.println("Квадрат числа "+number+" дорівнює "+sqrNumber);
     System.out.println("Об'єм циліндра з радіусом "+radius+" і висотою "+height+" дорівнює "+volume);
+    System.out.println("Масив чисел "+ Arrays.toString(arrayOfInt));
+    System.out.println("Сума всіх елементів масиву дорівнює "+sumFromArrayOfInt);
 }
 int getSquareOfNumber (int number ){
     return number * number;
 }
 double getVolume  (double height , double radius ){
     return Math.PI * Math.pow(radius, 2) * height;
+}
+int getSumOfAllIntInTheArray(int[] array){
+    int sum = 0;
+    for(int i = 0; i < array.length; i++){
+        sum += array[i];
+    }
+    return sum;
 }
